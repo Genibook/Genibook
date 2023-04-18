6538910f0f411461170c7b2ae6944f696781efa4
ed.xyz <eddietang2314@gmail.com>
2023-04-17 21:58:25 -0400

    sus

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:genibook/constants.dart';
+import 'package:genibook/navigator/swipes.dart';
 import 'package:genibook/screens/login.dart';
 import 'package:genibook/extensions/darkmode.dart';
 
@@ -14,18 +15,29 @@ class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
+  bool _showButton = false;
+
   @override
   void initState() {
+    super.initState();
+
     _controller = AnimationController(
       duration: const Duration(milliseconds: 5000),
       vsync: this,
     );
-    super.initState();
-    _controller.repeat(period: const Duration(seconds: 4));
+
+    _controller.repeat(period: const Duration(seconds: 2));
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _showButton = true;
+        _controller.forward();
+      });
+    });
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     _controller.dispose();
     super.dispose();
   }
@@ -56,6 +68,28 @@ class _SplashScreenState extends State<SplashScreen>
               Constants.tagline,
               style: Theme.of(context).textTheme.headline2,
               textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Opacity(
+              opacity: _showButton ? _controller.value : 0.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      //TODO: tos page etc
+                      .push(SlideToRightPageRoute(child: LoginPage()));
+                },
+                child: const SizedBox(
+                    height: 30,
+                    width: 120,
+                    child: Center(
+                      child: Text(
+                        "Let's hop in 🐈🎓",
+                        textAlign: TextAlign.center,
+                      ),
+                    )),
+              ),
             )
           ],
         ),
