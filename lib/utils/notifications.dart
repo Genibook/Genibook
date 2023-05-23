c755947ad1a5d3abe6535d059fe3e9b788fbaf70
ed.xyz <eddietang2314@gmail.com>
2023-05-22 20:23:41 -0400

    asdf

--- lib/utils/notifications.dart
+++ lib/utils/notifications.dart
@@ -1,4 +1,4 @@
-// import 'package:genibook/models/grades_class.dart';
+import 'package:genibook/models/grades_class.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:genibook/models/student_class.dart';
 import 'package:genibook/services/notification_service.dart';
@@ -6,12 +6,12 @@ import 'package:genibook/services/notification_service.dart';
 /// Sending a notification with a given student object that tells that user what grade changed
 Future<void> sendBGTaskNotification(
     Student newStudent, Student oldStudent) async {
-  // Grades newGrades = newStudent.grades;
-  // Grades oldGrades = oldStudent.grades;
+  Grades newGrades = newStudent.grades;
+  Grades oldGrades = oldStudent.grades;
 
-  // if (newGrades == oldGrades) {
-  //   return;
-  // }
+  if (newGrades == oldGrades) {
+    return;
+  }
 
   await NotificationService.showNotification(
     title: "${Emojis.animals_cat} Your grades changed, take a look!",
