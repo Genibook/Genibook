c82a0037793cce3fc934d80a8a14e4ac4e4ee257
Eddie <eddietang2314@gmail.com>
2023-05-28 11:52:17 -0400

    setup

--- android/app/src/main/kotlin/com/example/genibook/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/genibook/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.genibook.application
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
