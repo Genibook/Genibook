e78bccdd385a99e8e84f65bf8d49136107bdfd42
ed.xyz <eddietang2314@gmail.com>
2023-07-08 11:12:23 -0400

    isTodaySummer() function logic fix

--- lib/utils/dates.dart
+++ lib/utils/dates.dart
@@ -2,5 +2,6 @@ bool isTodaySummer() {
   DateTime now = DateTime.now();
   DateTime date = DateTime(now.year, now.month, now.day);
   DateTime summerStart = DateTime(now.year, 7, 1);
-  return date.isAfter(summerStart);
+  DateTime summerEnd = DateTime(now.year, 8, 20);
+  return date.isAfter(summerStart) && date.isBefore(summerEnd);
 }
